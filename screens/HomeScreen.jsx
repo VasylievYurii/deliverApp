62810093098dc6c98e635ce09deef4d674bb855c
VasylievYurii <uraplanetianin@gmail.com>
2023-12-18 21:46:11 +0200

    SafeAreaView-for-Android

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,11 +1,21 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import SafeViewAndroid from "../components/SafeViewAndroid/SafeViewAndroid";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
+    <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
       <Text className="text-red-500">HomeScreen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
